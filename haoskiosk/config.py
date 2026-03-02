02a5a3e479fc9576a03adaa22632d031a622affb
Mendel Greenberg <mendel@chabad360.me>
2026-03-02 10:10:33 -0800

    Add initial configuration settings for qutebrowser

---
+++ haoskiosk/config.py
@@ -0,0 +1,12 @@
+# pyright: reportUndefinedVariable=false
+config.load_autoconfig(False)
+c.statusbar.show = 'never'
+c.tabs.show = 'never'
+c.tabs.tabs_are_windows = True
+c.window.hide_decoration = True
+c.zoom.levels = ['150%']
+c.zoom.text_only = False
+c.zoom.default = '150%'
+config.unbind('<F11>', mode='normal')
+config.bind('<Escape>', 'mode-enter passthrough')
+config.unbind('<Shift-Escape>', mode='passthrough')
