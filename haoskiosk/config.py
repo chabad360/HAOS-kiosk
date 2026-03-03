418ae23f9ed72ae557af024dfa50782c577fd75a
Mendel Greenberg <mendel@chabad360.me>
2026-03-03 11:10:04 -0800

    Add dark color scheme and update key bindings

--- haoskiosk/config.py
+++ haoskiosk/config.py
@@ -7,6 +7,8 @@ c.window.hide_decoration = True
 c.zoom.levels = ['150%']
 c.zoom.text_only = False
 c.zoom.default = '150%'
+c.colors.webpage.preferred_color_scheme = 'dark'
 config.unbind('<F11>', mode='normal')
 config.bind('<Escape>', 'mode-enter passthrough')
 config.unbind('<Shift-Escape>', mode='passthrough')
+c.input.mode_override = 'passthrough'
